e241d83da235d9db8d47f5610df47da1c63229e1
IMCS2 <IMCS2@imcs1s-macbook-air-2.local>
2019-02-24 17:53:15 -0600

    completed spinner app

--- Spinner/ViewController.swift
+++ Spinner/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timeToPerformActivity:Double?
+    
+    @IBOutlet weak var input: UITextField!
+    @IBAction func Submit(_ sender: Any) {
+        if let cost = Double(input.text!) {
+            performIndication(pause: cost ?? 2.0)
+
+            print("The user entered a value price of \(cost)")
+        } else {
+            
+        }
+    }
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    func performIndication(pause:Double){
+    activityIndicator.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + pause) { // Change `2.0` to the desired number of     activityIndicator.startAnimating()
+            self.activityIndicator.stopAnimating()
+        }
+    
+        
+    
+    }
 
 }
 
